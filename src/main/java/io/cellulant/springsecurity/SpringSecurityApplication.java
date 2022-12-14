15b562794b717d80f38b1af918fbbf2222ef8a90
allanlimo02 <allanlimo02@gmail.com>
2022-12-14 13:03:52 +0300

    Updated role id

---
+++ src/main/java/io/cellulant/springsecurity/SpringSecurityApplication.java
@@ -0,0 +1,18 @@
+package io.cellulant.springsecurity;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityApplication.class, args);
+    }
+
+}
